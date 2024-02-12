b27966d556afbeb8f4833a2859d3b94b8364c1e1
ChasePoin <mcstarcraft04@gmail.com>
2024-02-12 17:04:11 -0600

    updated interface.py to reflect file name changes

--- interface.py
+++ interface.py
@@ -1,8 +1,8 @@
-import project1 as p
+import functions as func
 
 def main():
         fileName = input("Enter the name of the file you want to use: ")
-        p.scrape(fileName)
+        func.scrape(fileName)
 
 
 if __name__ == "__main__":
